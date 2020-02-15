be4d703d77c6824480b081ee4cf5693fd45e0afa
JosianeReis <josiane-reis@outlook.com>
2020-02-15 17:30:48 -0300

    Adding antDesign

--- src/modules/admin/pages/Home/index.tsx
+++ src/modules/admin/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { Button } from 'antd';
+import styles from './Home.module.scss';
 
 const Home: React.FC<RouteComponentProps> = () => (
-  <div>olá, sou a home de admin</div>
+  <>
+    <div>olá, sou a home de admin</div>
+    {/* <Button
+      type="primary"
+      // className={styles.button}
+    > */}
+    <Button type="primary" icon="search">Teste tema</Button>
+      {/* teste tema
+    </Button> */}
+  </>
 );
 
 export default Home;
