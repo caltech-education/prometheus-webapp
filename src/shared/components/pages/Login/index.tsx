5977dc48ffffc175b4e8a33fa9b718de87fdbc02
JosianeReis <josiane-reis@outlook.com>
2020-02-15 19:01:57 -0300

    Removing comment

--- src/shared/components/pages/Login/index.tsx
+++ src/shared/components/pages/Login/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { Link } from '@reach/router';
 
 import imageTeste from 'shared/assets/images/success.png';
-//enfia esse import absoluto no cu
+//enfia esse import absoluto no cu, weverton
 
 
-// To-Do --- resolver import por absolute path e import de modulos scss(erro)
 // To-Do --- verificar sobre atualizaçao de versoes de pacotes e se for o caso travar as versoes no package.json
 
 const Login = () => (
