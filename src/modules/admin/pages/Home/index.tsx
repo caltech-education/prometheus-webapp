67f36abe5d0a0ff93bc6a2bacbef08b4553c5b49
Weverton Souza <wevertonad@gmail.com>
2020-02-16 16:57:37 -0300

    Remove ant lib and added material google lib

--- src/modules/admin/pages/Home/index.tsx
+++ src/modules/admin/pages/Home/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { Button } from 'antd';
-// import styles from './Home.module.scss';
 
 const Home: React.FC<RouteComponentProps> = () => (
   <>
     <div>olá, sou a home de admin</div>
-    <Button type="primary" icon="search">Teste tema</Button>
   </>
 );
 
